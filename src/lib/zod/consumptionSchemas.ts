f4dab7071e571e1d377eb80105897946d045d9ab
LarsLrn <rotambus@googlemail.com>
2024-02-24 10:33:20 +0100

    Added schemas for heating and transportation

--- src/lib/zod/consumptionSchemas.ts
+++ src/lib/zod/consumptionSchemas.ts
@@ -1,13 +1,30 @@
 import { Consumption } from "@/models/firestore/consumption/consumption";
 import { ConsumptionElectricitySource } from "@/models/firestore/consumption/electricity/consumption-electricity-source";
+import { ConsumptionDistrictHeatingSource } from "@/models/firestore/consumption/heating/consumption-district-heating-source";
+import { ConsumptionHeatingFuel } from "@/models/firestore/consumption/heating/consumption-heating-fuel";
+import { ConsumptionTransportationPublicVehicleOccupancy } from "@/models/firestore/consumption/transportation/consumption-transportation-public-vehicle-occupancy";
+import { ConsumptionTransportationType } from "@/models/firestore/consumption/transportation/consumption-transportation-type";
 import { Timestamp } from "firebase/firestore";
 import { z } from "zod";
-import { consumptionSources } from "../constants";
+import { consumptionSources, publicVehicleOccupancies } from "../constants";
 
 const TimestampSchema = z.instanceof(Timestamp);
 
-const sources: ConsumptionElectricitySource[] =
+const electricitySources: ConsumptionElectricitySource[] =
     consumptionSources.electricity.map((source) => source.source);
+
+const heatingFuels: ConsumptionHeatingFuel[] = consumptionSources.heating.map(
+    (source) => source.source,
+);
+
+const districtHeatingSources: ConsumptionDistrictHeatingSource[] =
+    consumptionSources.districtHeating.map((source) => source.source);
+
+const transportationTypes: ConsumptionTransportationType[] =
+    consumptionSources.transportation.map((source) => source.source);
+
+const transportationPublicVehicleOccupancies: ConsumptionTransportationPublicVehicleOccupancy[] =
+    publicVehicleOccupancies.map((occupancy) => occupancy.key);
 
 export const electricityFormSchema = (
     t: (arg: string) => string,
@@ -18,11 +35,65 @@ export const electricityFormSchema = (
         }),
         category: z.literal("electricity"),
         electricity: z.object({
-            electricitySource: z.enum([sources[0], ...sources]),
+            electricitySource: z.enum([
+                electricitySources[0],
+                ...electricitySources,
+            ]),
             costs: z.coerce.number().max(1000000),
             householdSize: z.coerce.number().min(1).max(100),
             startDate: TimestampSchema,
             endDate: TimestampSchema,
+        }),
+        description: z.string().max(1000).optional(),
+        createdAt: TimestampSchema,
+    });
+
+export const heatingFormSchema = (
+    t: (arg: string) => string,
+): z.ZodType<Consumption> =>
+    z.object({
+        value: z.coerce.number().max(1000000, {
+            message: t("app.validation.error.validValue"),
+        }),
+        category: z.literal("heating"),
+        heating: z.object({
+            heatingFuel: z.enum([heatingFuels[0], ...heatingFuels]),
+            districtHeatingSource: z
+                .enum([districtHeatingSources[0], ...districtHeatingSources])
+                .optional(),
+            costs: z.coerce.number().max(1000000),
+            householdSize: z.coerce.number().min(1).max(100),
+            startDate: TimestampSchema,
+            endDate: TimestampSchema,
+        }),
+        description: z.string().max(1000).optional(),
+        createdAt: TimestampSchema,
+    });
+
+export const transportationFormSchema = (
+    t: (arg: string) => string,
+): z.ZodType<Consumption> =>
+    z.object({
+        value: z.coerce.number().max(1000000, {
+            message: t("app.validation.error.validValue"),
+        }),
+        category: z.literal("transportation"),
+        transportation: z.object({
+            transportationType: z.enum([
+                transportationTypes[0],
+                ...transportationTypes,
+            ]),
+            privateVehicleOccupancy: z.coerce
+                .number()
+                .min(1)
+                .max(15)
+                .optional(),
+            publicVehicleOccupancy: z.enum([
+                transportationPublicVehicleOccupancies[0],
+                ...transportationPublicVehicleOccupancies,
+            ]),
+            dateOfTravel: TimestampSchema,
+            dateOfTravelEnd: TimestampSchema.optional(),
         }),
         description: z.string().max(1000).optional(),
         createdAt: TimestampSchema,
