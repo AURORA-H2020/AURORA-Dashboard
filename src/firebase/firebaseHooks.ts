35a906129671321eb84493257916f8fa56372b3d
LarsLrn <rotambus@googlemail.com>
2024-02-26 22:44:31 +0100

    Added custom firebase hooks

---
+++ src/firebase/firebaseHooks.ts
@@ -0,0 +1,101 @@
+import firebase_app from "@/firebase/config";
+import { ConsumptionWithID } from "@/models/extensions";
+import { ConsumptionSummary } from "@/models/firestore/consumption-summary/consumption-summary";
+import { Consumption } from "@/models/firestore/consumption/consumption";
+import { User as FirebaseUser } from "@/models/firestore/user/user";
+import { User } from "firebase/auth";
+import { collection, doc, getFirestore, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { FirebaseConstants } from "./firebase-constants";
+
+const firestore = getFirestore(firebase_app);
+
+export const useFetchUserData = (user: User, loading: boolean, router) => {
+    const [userData, setUserData] = useState<FirebaseUser | null>(null);
+    const [isLoadingUserData, setIsLoadingUserData] = useState<boolean>(true);
+
+    useEffect(() => {
+        if (loading) return;
+
+        if (!user) {
+            router.replace("/");
+            return;
+        }
+
+        const docRef = doc(
+            firestore,
+            FirebaseConstants.collections.users.name,
+            user.uid,
+        );
+        const unsubscribe = onSnapshot(docRef, (docSnapshot) => {
+            if (docSnapshot.exists()) {
+                console.log("Document data:", docSnapshot.data());
+                setUserData(docSnapshot.data() as FirebaseUser);
+                setIsLoadingUserData(false);
+            } else {
+                console.log("No such document!");
+            }
+        });
+
+        return unsubscribe;
+    }, [user, router, loading]);
+
+    return { userData, isLoadingUserData };
+};
+
+export const useFetchUserConsumptions = (user: User) => {
+    const [userConsumptions, setUserConsumptions] = useState<
+        ConsumptionWithID[]
+    >([]);
+
+    useEffect(() => {
+        if (!user) return;
+
+        const collectionRef = collection(
+            firestore,
+            FirebaseConstants.collections.users.name,
+            user.uid,
+            FirebaseConstants.collections.users.consumptions.name,
+        );
+        const unsubscribe = onSnapshot(collectionRef, (querySnapshot) => {
+            const consumptions = querySnapshot.docs.map((doc) => ({
+                ...(doc.data() as Consumption),
+                id: doc.id,
+            }));
+            setUserConsumptions(consumptions);
+        });
+
+        return unsubscribe;
+    }, [user]);
+
+    return userConsumptions;
+};
+
+export const useFetchUserConsumptionSummaries = (user: User) => {
+    const [userConsumptionSummaries, setUserConsumptionSummaries] = useState<
+        ConsumptionSummary[]
+    >([]);
+
+    useEffect(() => {
+        if (!user) return;
+
+        const collectionRef = collection(
+            firestore,
+            FirebaseConstants.collections.users.name,
+            user.uid,
+            FirebaseConstants.collections.users.consumptionSummaries.name,
+        );
+
+        const unsubscribe = onSnapshot(collectionRef, (querySnapshot) => {
+            const fetchedSummaries = querySnapshot.docs.map(
+                (doc) => doc.data() as ConsumptionSummary,
+            );
+
+            setUserConsumptionSummaries(fetchedSummaries);
+        });
+
+        return unsubscribe;
+    }, [user]);
+
+    return userConsumptionSummaries;
+};
