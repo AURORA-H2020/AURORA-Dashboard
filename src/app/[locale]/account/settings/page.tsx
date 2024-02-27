f48cd7cf911a50251a35f7694228b642b8b56e2f
LarsLrn <rotambus@googlemail.com>
2024-02-27 18:00:03 +0100

    Improved settings page

--- src/app/[locale]/account/settings/page.tsx
+++ src/app/[locale]/account/settings/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import ConsumptionTableRow from "@/components/app/consumptions/consumptionTableRow";
+import { ChangeEmailForm } from "@/components/app/user/changeEmailForm";
+import { ChangePasswordForm } from "@/components/app/user/changePasswordForm";
 import EditUserDataModal from "@/components/app/user/editUserDataModal";
 import {
     AlertDialog,
@@ -23,11 +25,9 @@ import {
 import {
     Dialog,
     DialogContent,
-    DialogFooter,
     DialogHeader,
     DialogTitle,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
 import LoadingSpinner from "@/components/ui/loading";
 import { Table, TableBody } from "@/components/ui/table";
 import { useAuthContext } from "@/context/AuthContext";
@@ -35,6 +35,7 @@ import { useFirebaseData } from "@/context/FirebaseContext";
 import { deleteAccount } from "@/firebase/firestore/deleteAccount";
 import { downloadUserData } from "@/firebase/firestore/downloadUserData";
 import { Flex, Grid } from "@radix-ui/themes";
+import { User } from "firebase/auth";
 import { useState } from "react";
 import { toast } from "sonner";
 
@@ -80,6 +81,22 @@ function UserSettings(): JSX.Element {
             toast.error("Your data could not be downloaded");
         } finally {
             setDownloading(false);
+        }
+    };
+
+    const handleUserDelete = async (user: User) => {
+        const { success, requiresReauth } = await deleteAccount(user);
+
+        if (success) {
+            toast.success("Your account has been deleted");
+            setDeleteAlertOpen(false);
+        } else {
+            if (requiresReauth) {
+                toast.error("Please reauthenticate to update your password", {
+                    duration: 10000,
+                    description: "You can do this by logging out and back in.",
+                });
+            } else toast.error("An error occurred updating your password");
         }
     };
 
@@ -223,16 +240,7 @@ function UserSettings(): JSX.Element {
                     <DialogHeader>
                         <DialogTitle>Change Email</DialogTitle>
                     </DialogHeader>
-                    <Flex direction={"column"} className={"gap-4"}>
-                        <Input></Input>
-                    </Flex>
-                    <DialogFooter className="flex sm:justify-between">
-                        {/* TODO: Add button functionality */}
-                        <Flex className="space-x-2">
-                            <Button type="submit">Cancel</Button>
-                            <Button type="submit">Save</Button>
-                        </Flex>
-                    </DialogFooter>
+                    <ChangeEmailForm onFormSubmit={closeModal} />
                 </DialogContent>
             </Dialog>
 
@@ -241,16 +249,7 @@ function UserSettings(): JSX.Element {
                     <DialogHeader>
                         <DialogTitle>Change Password</DialogTitle>
                     </DialogHeader>
-                    <Flex direction={"column"} className={"gap-4"}>
-                        <Input></Input>
-                    </Flex>
-                    <DialogFooter className="flex sm:justify-between">
-                        {/* TODO: Add button functionality */}
-                        <Flex className="space-x-2">
-                            <Button type="submit">Cancel</Button>
-                            <Button type="submit">Save</Button>
-                        </Flex>
-                    </DialogFooter>
+                    <ChangePasswordForm onFormSubmit={closeModal} />
                 </DialogContent>
             </Dialog>
 
@@ -274,7 +273,7 @@ function UserSettings(): JSX.Element {
                         </AlertDialogCancel>
                         <AlertDialogAction
                             variant="destructive"
-                            onClick={deleteAccount}
+                            onClick={() => handleUserDelete(user)}
                         >
                             Delete Account
                         </AlertDialogAction>
