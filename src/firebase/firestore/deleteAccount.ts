a080f7a99fb57872065987503e0381a01fbbacff
LarsLrn <rotambus@googlemail.com>
2024-02-27 18:03:41 +0100

    Refactored account deletion

--- src/firebase/firestore/deleteAccount.ts
+++ src/firebase/firestore/deleteAccount.ts
@@ -1,25 +1,30 @@
-import firebase_app from "@/firebase/config";
-import { deleteUser, getAuth } from "firebase/auth";
+import { User, deleteUser } from "firebase/auth";
 
 /**
  * Deletes the current user account if it exists, and handles success and error cases.
  *
  * @return {Promise<void>} A promise that resolves once the account is deleted, or rejects with an error.
  */
-export const deleteAccount = async () => {
-    const auth = getAuth(firebase_app);
-    const user = auth.currentUser;
+export const deleteAccount = async (user: User | null) => {
+    let success = false;
+    let requiresReauth = false;
 
     if (user) {
-        try {
-            await deleteUser(user);
-            console.log("User account deleted successfully.");
-            // Redirect to login/signup page or display a message if needed
-        } catch (error) {
-            console.error("Error deleting user account:", error);
-            // Display an error message to the user if needed
-        }
+        await deleteUser(user)
+            .then(() => {
+                console.log("User account deleted");
+                success = true;
+            })
+            .catch((error) => {
+                if (error.code === "auth/requires-recent-login") {
+                    requiresReauth = true;
+                } else {
+                    console.error("Error deleting user account: ", error);
+                }
+            });
     } else {
-        console.log("No user is currently signed in.");
+        throw new Error("User is not logged in.");
     }
+
+    return { success, requiresReauth };
 };
