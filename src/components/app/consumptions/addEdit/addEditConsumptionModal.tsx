3f500053175918269c4746026e4fcafb1e1c34d4
LarsLrn <rotambus@googlemail.com>
2024-02-28 20:42:16 +0100

    Fixed warning using forwardRef

--- src/components/app/consumptions/addEdit/addEditConsumptionModal.tsx
+++ src/components/app/consumptions/addEdit/addEditConsumptionModal.tsx
@@ -11,77 +11,90 @@ import { useState } from "react";
 import ElectricityForm from "./electricityForm";
 import HeatingForm from "./heatingForm";
 import TransportationForm from "./transportationForm";
+import React from "react";
 
-export default function AddEditConsumptionModal({
-    consumption,
-    children,
-}: {
-    consumption?: ConsumptionWithID;
-    children: React.ReactNode;
-}) {
-    const t = useTranslations();
+const AddEditConsumptionModal = React.forwardRef(
+    (props: { consumption?: ConsumptionWithID; children: React.ReactNode }) => {
+        const { consumption, children } = props;
 
-    const [currentTab, setCurrentTab] = useState<ConsumptionCategory>(
-        consumption?.category || "electricity",
-    );
-    const [isModalOpen, setIsModalOpen] = useState(false);
+        const t = useTranslations();
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
+        const [currentTab, setCurrentTab] = useState<ConsumptionCategory>(
+            consumption?.category || "electricity",
+        );
+        const [isModalOpen, setIsModalOpen] = useState(false);
 
-    return (
-        <>
-            <div onClick={() => setIsModalOpen(true)}>{children}</div>
-            <Dialog open={isModalOpen} onOpenChange={handleCloseModal}>
-                <DialogContent className="sm:max-w-lg p-0">
-                    <ScrollArea className="max-h-[80vh] p-6">
-                        <Tabs
-                            value={currentTab}
-                            onValueChange={(value) =>
-                                setCurrentTab(value as ConsumptionCategory)
-                            }
-                        >
-                            {!consumption && (
-                                <div className="overflow-x-auto">
-                                    <TabsList className="w-full my-6">
-                                        {consumptionMapping.map(
-                                            (consumption) => (
-                                                <TabsTrigger
-                                                    key={consumption.category}
-                                                    value={consumption.category}
-                                                    className="w-full"
-                                                >
-                                                    {t(consumption.label)}
-                                                </TabsTrigger>
-                                            ),
-                                        )}
-                                    </TabsList>
-                                </div>
-                            )}
+        const handleCloseModal = () => {
+            setIsModalOpen(false);
+        };
 
-                            <TabsContent value="electricity" className="p-2">
-                                <ElectricityForm
-                                    consumption={consumption}
-                                    onConsumptionAdded={handleCloseModal}
-                                />
-                            </TabsContent>
-                            <TabsContent value="heating" className="p-2">
-                                <HeatingForm
-                                    consumption={consumption}
-                                    onConsumptionAdded={handleCloseModal}
-                                />
-                            </TabsContent>
-                            <TabsContent value="transportation" className="p-2">
-                                <TransportationForm
-                                    consumption={consumption}
-                                    onConsumptionAdded={handleCloseModal}
-                                />
-                            </TabsContent>
-                        </Tabs>
-                    </ScrollArea>
-                </DialogContent>
-            </Dialog>
-        </>
-    );
-}
+        return (
+            <>
+                <div onClick={() => setIsModalOpen(true)}>{children}</div>
+                <Dialog open={isModalOpen} onOpenChange={handleCloseModal}>
+                    <DialogContent className="sm:max-w-lg p-0">
+                        <ScrollArea className="max-h-[80vh] p-6">
+                            <Tabs
+                                value={currentTab}
+                                onValueChange={(value) =>
+                                    setCurrentTab(value as ConsumptionCategory)
+                                }
+                            >
+                                {!consumption && (
+                                    <div className="overflow-x-auto">
+                                        <TabsList className="w-full my-6">
+                                            {consumptionMapping.map(
+                                                (consumption) => (
+                                                    <TabsTrigger
+                                                        key={
+                                                            consumption.category
+                                                        }
+                                                        value={
+                                                            consumption.category
+                                                        }
+                                                        className="w-full"
+                                                    >
+                                                        {t(consumption.label)}
+                                                    </TabsTrigger>
+                                                ),
+                                            )}
+                                        </TabsList>
+                                    </div>
+                                )}
+
+                                <TabsContent
+                                    value="electricity"
+                                    className="p-2"
+                                >
+                                    <ElectricityForm
+                                        consumption={consumption}
+                                        onConsumptionAdded={handleCloseModal}
+                                    />
+                                </TabsContent>
+                                <TabsContent value="heating" className="p-2">
+                                    <HeatingForm
+                                        consumption={consumption}
+                                        onConsumptionAdded={handleCloseModal}
+                                    />
+                                </TabsContent>
+                                <TabsContent
+                                    value="transportation"
+                                    className="p-2"
+                                >
+                                    <TransportationForm
+                                        consumption={consumption}
+                                        onConsumptionAdded={handleCloseModal}
+                                    />
+                                </TabsContent>
+                            </Tabs>
+                        </ScrollArea>
+                    </DialogContent>
+                </Dialog>
+            </>
+        );
+    },
+);
+
+AddEditConsumptionModal.displayName = "AddEditConsumptionModal";
+
+export default AddEditConsumptionModal;
--- src/components/app/recurringConsumptions/addEdit/addEditRecurringConsumptionModal.tsx
+++ src/components/app/recurringConsumptions/addEdit/addEditRecurringConsumptionModal.tsx
@@ -7,50 +7,50 @@ import {
     DialogTitle,
 } from "@/components/ui/dialog";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { consumptionMapping } from "@/lib/constants";
 import { RecurringConsumptionWithID } from "@/models/extensions";
-import { ConsumptionCategory } from "@/models/firestore/consumption/consumption-category";
-import { useTranslations } from "next-intl";
 import { useState } from "react";
 import RecurringTransportationForm from "./recurringTransportationForm";
+import React from "react";
 
-export default function AddEditRecurringConsumptionModal({
-    recurringConsumption,
-    children,
-}: {
-    recurringConsumption?: RecurringConsumptionWithID;
-    children: React.ReactNode;
-}) {
-    const t = useTranslations();
+const AddEditRecurringConsumptionModal = React.forwardRef(
+    (props: {
+        recurringConsumption?: RecurringConsumptionWithID;
+        children: React.ReactNode;
+    }) => {
+        const { recurringConsumption, children } = props;
 
-    const [currentTab, setCurrentTab] = useState<ConsumptionCategory>(
-        recurringConsumption?.category || "transportation",
-    );
-    const [isModalOpen, setIsModalOpen] = useState(false);
+        const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
+        const handleCloseModal = () => {
+            setIsModalOpen(false);
+        };
 
-    return (
-        <>
-            <div onClick={() => setIsModalOpen(true)}>{children}</div>
-            <Dialog open={isModalOpen} onOpenChange={handleCloseModal}>
-                <DialogContent className="sm:max-w-lg p-0">
-                    <ScrollArea className="max-h-[80vh] p-6">
-                        <DialogHeader>
-                            <DialogTitle>Recurring Transportation</DialogTitle>
-                        </DialogHeader>
-                        <div className="p-2">
-                            <RecurringTransportationForm
-                                recurringConsumption={recurringConsumption}
-                                onFormSubmit={handleCloseModal}
-                            />
-                        </div>
-                    </ScrollArea>
-                </DialogContent>
-            </Dialog>
-        </>
-    );
-}
+        return (
+            <>
+                <div onClick={() => setIsModalOpen(true)}>{children}</div>
+                <Dialog open={isModalOpen} onOpenChange={handleCloseModal}>
+                    <DialogContent className="sm:max-w-lg p-0">
+                        <ScrollArea className="max-h-[80vh] p-6">
+                            <DialogHeader>
+                                <DialogTitle>
+                                    Recurring Transportation
+                                </DialogTitle>
+                            </DialogHeader>
+                            <div className="p-2">
+                                <RecurringTransportationForm
+                                    recurringConsumption={recurringConsumption}
+                                    onFormSubmit={handleCloseModal}
+                                />
+                            </div>
+                        </ScrollArea>
+                    </DialogContent>
+                </Dialog>
+            </>
+        );
+    },
+);
+
+AddEditRecurringConsumptionModal.displayName =
+    "AddEditRecurringConsumptionModal";
+
+export default AddEditRecurringConsumptionModal;
