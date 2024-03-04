65ee7cdef013abaae8afdde5bbfac494655d52d7
LarsLrn <rotambus@googlemail.com>
2024-03-04 18:21:44 +0100

    Added support for visibility switch and input unit

--- src/components/formItems/formInputField.tsx
+++ src/components/formItems/formInputField.tsx
@@ -4,9 +4,11 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utilities";
+import { useState } from "react";
 import { ControllerRenderProps } from "react-hook-form";
 import { FormItem, FormLabel } from "../ui/form";
-import { cn } from "@/lib/utilities";
+import { Switch } from "../ui/switch";
 
 type InputTypes = "number" | "text" | "email";
 
@@ -17,6 +19,8 @@ const FormInputField = ({
     label,
     description,
     className,
+    showSwitch,
+    unit,
 }: {
     field: ControllerRenderProps<any, any>;
     inputType: InputTypes;
@@ -24,21 +28,61 @@ const FormInputField = ({
     label?: string;
     description?: string;
     className?: string;
-}) => (
-    <FormItem className={cn(className)}>
-        {label && <FormLabel>{label}</FormLabel>}
-        {description && <FormDescription>{description}</FormDescription>}
-        <FormControl>
-            <Input
-                type={inputType}
-                placeholder={placeholder}
-                {...field}
-                value={field.value ?? ""}
-            />
-        </FormControl>
+    showSwitch?: boolean;
+    unit?: string;
+}) => {
+    const [visible, setVisible] = useState(
+        showSwitch ? (field.value ? true : false) : true,
+    );
 
-        <FormMessage />
-    </FormItem>
-);
+    const handleCheckedChange = (state: boolean) => {
+        if (state) {
+            setVisible(true);
+        } else {
+            setVisible(false);
+            field.value = undefined;
+            field.onChange(undefined);
+        }
+    };
+
+    return (
+        <FormItem className={cn(className)}>
+            {showSwitch ? (
+                <div className="flex items-center space-x-2">
+                    <Switch
+                        checked={visible}
+                        onCheckedChange={handleCheckedChange}
+                        id={field.name}
+                    />
+                    <FormLabel htmlFor={field.name}>{label}</FormLabel>
+                </div>
+            ) : (
+                label && <FormLabel>{label}</FormLabel>
+            )}
+            {description && <FormDescription>{description}</FormDescription>}
+            {visible && (
+                <FormControl>
+                    <div className="relative">
+                        <Input
+                            type={inputType}
+                            placeholder={placeholder}
+                            {...field}
+                            value={field.value ?? ""}
+                        />
+                        {unit && (
+                            <div className="absolute inset-y-0 right-0 pr-0 flex items-center cursor-pointer">
+                                <div className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm text-muted-foreground font-mono border-l min-w-14 h-full p-1 rounded-l-none">
+                                    {unit}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </FormControl>
+            )}
+
+            <FormMessage />
+        </FormItem>
+    );
+};
 
 export default FormInputField;
