e30e331c253e9c0b6177ab0cbcf80f503f2a294c
LarsLrn <rotambus@googlemail.com>
2024-05-15 11:05:26 +0200

    Added utility function to check optional fields with zod

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -12,6 +12,7 @@ import { clsx, type ClassValue } from "clsx";
 import convert from "convert";
 import { useFormatter } from "next-intl";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 /**
  * Automatically added by shadcn/ui
@@ -357,4 +358,59 @@ export function useConvertUnit(
             return `${this.number} ${this.unit}`;
         },
     };
+}
+
+/**
+ * Extracts the base schema from the given Zod schema.
+ *
+ * @param {z.ZodTypeAny} schema - The Zod schema to extract the base schema from.
+ * @return {z.ZodObject<any> | null} The base schema if found, otherwise null.
+ */
+function extractBaseSchema(schema: z.ZodTypeAny): z.ZodObject<any> | null {
+    if (schema instanceof z.ZodObject) {
+        return schema;
+    }
+    if (schema instanceof z.ZodOptional) {
+        return extractBaseSchema(schema._def.innerType);
+    }
+    if (schema instanceof z.ZodEffects && schema._def.schema) {
+        return extractBaseSchema(schema._def.schema);
+    }
+    return null;
+}
+
+/**
+ * Checks if a field is required in the given schema.
+ *
+ * @param {z.ZodType<any>} schema - The schema to check against.
+ * @param {string} fieldPath - The name of the field to check.
+ * @return {boolean} Indicates if the field is required.
+ */
+export function isFieldRequired(
+    schema: z.ZodTypeAny,
+    fieldPath: string,
+): boolean {
+    const baseSchema = extractBaseSchema(schema);
+    if (!baseSchema) return true;
+
+    const shape = baseSchema._def.shape();
+    const keys = fieldPath.split(".");
+
+    let currentField = shape;
+    for (const key of keys) {
+        if (!currentField[key]) {
+            return true;
+        }
+
+        if (
+            currentField[key] instanceof z.ZodObject ||
+            currentField[key] instanceof z.ZodEffects
+        ) {
+            currentField = extractBaseSchema(currentField[key])?._def.shape();
+        } else {
+            return !(currentField[key] instanceof z.ZodOptional);
+        }
+    }
+
+    return true;
 }
