305a97d808f9bbc59c8b87d35bfa42bb02f085da
LarsLrn <rotambus@googlemail.com>
2024-09-23 15:15:25 +0200

    Added dateToKebabCase utility function

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -430,3 +430,11 @@ export function isFieldRequired(
 
   return true;
 }
+
+export const dateToKebabCase = (date: Date): string => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+};
