82a2818f302ee518bef379b6f99b502309b94c69
LarsLrn <rotambus@googlemail.com>
2024-10-17 22:14:47 +0200

    Improved data handling and chart display

--- src/app/[locale]/account/pv/page.tsx
+++ src/app/[locale]/account/pv/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { PlaceholderCard } from "@/components/app/common/placeholderCard";
 import { PvDataChart } from "@/components/pv-data/charts/pvDataChart";
 import { PvPanelDetails } from "@/components/pv-data/panels/pvPanelDetails";
 import { PvPanelInvestments } from "@/components/pv-data/panels/pvPanelInvestments";
@@ -10,6 +11,12 @@ import { ChartConfig } from "@/components/ui/chart";
 import { Label } from "@/components/ui/label";
 import { LoadingSpinner } from "@/components/ui/loading";
 import { Switch } from "@/components/ui/switch";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import {
   useFetchPlantPvData,
   useFetchPvPlants,
@@ -18,12 +25,18 @@ import { Link } from "@/i18n/routing";
 import { useCreateQueryString } from "@/lib/hooks/useCreateQueryString";
 import { useFirebaseData } from "@/providers/context/firebaseContext";
 import { Heading } from "@radix-ui/themes";
-import { useFormatter } from "next-intl";
+import { CircleHelpIcon, ZapOffIcon } from "lucide-react";
+import { useFormatter, useTranslations } from "next-intl";
 import { useState } from "react";
 
 const PvPage = () => {
+  const t = useTranslations();
   const format = useFormatter();
+
   const [showLifetime, setShowLifetime] = useState(false);
+  const [productionDisplay, setProductionDisplay] = useState<
+    "personal" | "total"
+  >("personal");
   const createQueryString = useCreateQueryString();
 
   const { userCityData, isLoadingUserCityData, pvInvestments } =
@@ -42,12 +55,14 @@ const PvPage = () => {
   const chartData = pvData
     ?.map((d) => ({
       ...d,
-      investEp:
-        ((pvInvestments
-          ?.filter((i) => i.investmentDate <= d.date)
-          .reduce((acc, { share }) => acc + share, 0) ?? 0) /
-          100) *
-        d.Ep,
+      Ep:
+        productionDisplay === "total"
+          ? d.Ep
+          : ((pvInvestments
+              ?.filter((i) => i.investmentDate <= d.date)
+              .reduce((acc, { share }) => acc + share, 0) ?? 0) /
+              100) *
+            d.Ep,
       pretty_date: format.dateTime(d.date.toDate(), {
         month: "numeric",
         day: "numeric",
@@ -58,17 +73,26 @@ const PvPage = () => {
 
   const chartConfig = {
     Ep: {
-      label: "Total Production",
+      label:
+        productionDisplay === "personal"
+          ? t("app.pv.yourProduction")
+          : t("app.pv.totalProduction"),
       color: "hsl(var(--chart-1))",
-    },
-    investEp: {
-      label: "Your Production",
-      color: "hsl(var(--chart-2))",
     },
   } satisfies ChartConfig;
 
   if (isLoadingUserCityData || isLoadingPvPlants) return <LoadingSpinner />;
-  if (!userCityData || !chartData || !pvPlant) return <div>Unsupported</div>;
+  if (
+    !userCityData ||
+    !chartData ||
+    !pvPlant ||
+    userCityData.hasPhotovoltaics === false
+  )
+    return (
+      <PlaceholderCard Icon={ZapOffIcon}>
+        {t("dashboard.pv.unsupportedRegionMessage")}
+      </PlaceholderCard>
+    );
 
   return (
     <>
@@ -84,9 +108,15 @@ const PvPage = () => {
             {chartData && firstInvestmentDate && (
               <PvDataGrid.DataPanel>
                 <ProductionSummary
-                  title="Production"
+                  title={t("common.production")}
                   production={chartData.reduce((n, { Ep }) => n + Ep, 0)}
-                  productionStart={chartData[0].date.toDate()}
+                  productionStart={
+                    showLifetime
+                      ? firstInvestmentDate.toDate()
+                      : new Date(
+                          new Date().getTime() - 30 * 24 * 60 * 60 * 1000,
+                        )
+                  }
                 />
               </PvDataGrid.DataPanel>
             )}
@@ -94,24 +124,20 @@ const PvPage = () => {
         }
         footer={
           <PvDataGrid.Footer>
-            <div className="items-top flex justify-between gap-4">
-              <p className="text-sm text-muted-foreground">
-                Your investment in your local AURORA solar power installations
-                helps reduce your carbon footprint. Here you can see a breakdown
-                of the total energy produced by the installation compared to
-                your share. Your share is the energy produced through the help
-                of your investment and is automatically added as an offset to
-                your profile at the end of each month.
-              </p>
+            <div className="items-top flex flex-col justify-between gap-4">
+              <div>
+                <span className="text-lg font-semibold">
+                  {t("app.pv.featureGuide.title")}
+                </span>
+                <p className="text-sm text-muted-foreground">
+                  {t("app.pv.featureGuide.body")}
+                </p>
+              </div>
               <Button asChild variant="outline" size="sm">
                 <Link
-                  href={
-                    `/pv-data` +
-                    "?" +
-                    createQueryString("site", pvPlant.plantId)
-                  }
+                  href={`/pv-data?${createQueryString("site", pvPlant.plantId)}`}
                 >
-                  See full data
+                  {t("app.pv.seeFullData")}
                 </Link>
               </Button>
             </div>
@@ -120,30 +146,59 @@ const PvPage = () => {
       >
         {firstInvestmentDate && (
           <>
-            <div className="mb-6 flex items-center justify-between">
+            <div className="mb-6 flex flex-col gap-4">
               <Heading>
                 {showLifetime
-                  ? `Production since ${format.dateTime(firstInvestmentDate.toDate(), { year: "numeric", month: "long", day: "numeric" })}`
-                  : "Production in the last 30 days"}{" "}
+                  ? `${t("common.productionSince")} ${format.dateTime(firstInvestmentDate.toDate(), { year: "numeric", month: "long", day: "numeric" })}`
+                  : t("app.pv.productionInLast30Days")}
               </Heading>
-              <div className="flex items-center space-x-2">
-                <Label htmlFor="show-lifetime">Show lifetime</Label>
-                <Switch
-                  checked={showLifetime}
-                  onCheckedChange={setShowLifetime}
-                  id="show-lifetime"
-                />
+              <div className="mb-4 flex items-center justify-between">
+                <Tabs
+                  value={productionDisplay}
+                  onValueChange={(value) =>
+                    setProductionDisplay(value as "total" | "personal")
+                  }
+                >
+                  <TabsList>
+                    <TabsTrigger value="personal">
+                      {t("app.pv.yourProduction")}
+                    </TabsTrigger>
+                    <TabsTrigger value="total">
+                      {t("app.pv.totalProduction")}
+                    </TabsTrigger>
+                  </TabsList>
+                </Tabs>
+                <div className="flex items-center space-x-2">
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger className="text-muted-foreground">
+                      <CircleHelpIcon className="size-5" />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{t("app.pv.showProductionSinceFirstInvestment")}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                  <Label htmlFor="show-lifetime">
+                    {t("app.pv.showLifetime")}
+                  </Label>
+                  <Switch
+                    checked={showLifetime}
+                    onCheckedChange={setShowLifetime}
+                    id="show-lifetime"
+                  />
+                </div>
               </div>
             </div>
             {chartData && (
-              <PvDataChart
-                chartType={showLifetime ? "line" : "bar"}
-                chartData={chartData}
-                chartConfig={chartConfig}
-                xDataKey="pretty_date"
-                unit="kWh"
-                decimals={2}
-              />
+              <>
+                <PvDataChart
+                  chartType={showLifetime ? "line" : "bar"}
+                  chartData={chartData}
+                  chartConfig={chartConfig}
+                  xDataKey="pretty_date"
+                  unit="kWh"
+                  decimals={2}
+                />
+              </>
             )}
           </>
         )}
