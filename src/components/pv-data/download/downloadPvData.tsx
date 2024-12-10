ed1739cee4bebc5b83486f0571a0b0a0fec0be8e
LarsLrn <rotambus@googlemail.com>
2024-12-10 16:40:13 +0100

    Improved PV data export

--- src/components/pv-data/download/downloadPvData.tsx
+++ src/components/pv-data/download/downloadPvData.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  citiesMappings,
+  countriesMapping,
+} from "@/lib/constants/common-constants";
 import { downloadJsonAsFile } from "@/lib/utilities";
+import { useFirebaseData } from "@/providers/context/firebaseContext";
 import { DownloadIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
+import { useSearchParams } from "next/navigation";
 
 const DownloadPvData = ({
   data,
@@ -14,8 +20,36 @@ const DownloadPvData = ({
 }) => {
   const t = useTranslations();
 
+  const searchParams = useSearchParams();
+  const site = searchParams.get("site");
+
+  const { pvPlants, isLoadingPvPlants } = useFirebaseData();
+
+  if (!pvPlants && isLoadingPvPlants) return null;
+
+  const siteDetails = pvPlants && pvPlants.find((s) => s.plantId === site);
+
+  const jsonData = {
+    city: t(citiesMappings.find((c) => c.ID === siteDetails?.city)?.name),
+    country: t(
+      countriesMapping.find((c) => c.ID === siteDetails?.country)?.name,
+    ),
+    countryCode: countriesMapping.find((c) => c.ID === siteDetails?.country)
+      ?.code,
+    name: siteDetails?.name,
+    installationDate: siteDetails?.installationDate,
+    plantId: siteDetails?.plantId,
+    active: siteDetails?.active,
+    technology: siteDetails?.technology,
+    pricePerShare: siteDetails?.pricePerShare,
+    kwPerShare: siteDetails?.kwPerShare,
+    manufacturer: siteDetails?.manufacturer,
+    capacity: siteDetails?.capacity,
+    data,
+  };
+
   const downloadData = () => {
-    downloadJsonAsFile(data, filename);
+    downloadJsonAsFile(jsonData, filename);
   };
 
   return (
