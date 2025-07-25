7a8144fd91eeac6916be90ba9bf672d89eaccac1
LarsLrn <rotambus@googlemail.com>
2025-07-25 12:30:34 +0200

    fix: remove manually updating recommendations
    * This was only used for testing and is no longer needed

--- src/components/app/recommendations/updateRecommendationsSection.tsx
+++ src/components/app/recommendations/updateRecommendationsSection.tsx
@@ -1,51 +1,20 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { updateRecommendations } from "@/firebase/firestore/test-update-recommendations";
-import { useState } from "react";
-import { toast } from "sonner";
 import { BorderBox } from "../common/borderBox";
 
-interface FirebaseResponse {
-  success: boolean;
-  error?: unknown;
-}
-
 const UpdateRecommendationsSection = () => {
-  const [updatingRecommendations, setUpdatingRecommendations] = useState(false);
-
-  const updateRecommendationsWrapper = async () => {
-    setUpdatingRecommendations(true);
-
-    toast.promise(updateRecommendations() as Promise<FirebaseResponse>, {
-      loading: "Updating recommendations...",
-      success: "Recommendations updated successfully.",
-      error: (error) => `Error updating recommendations: ${error}`,
-      finally: () => {
-        setUpdatingRecommendations(false);
-      },
-    });
-  };
-
   return (
     <BorderBox className="w-full">
       <div className="flex flex-col gap-4">
         <div>
           <p>
-            Select a Recommendation or update your recommendations manually.
+            To help you improve your energy behaviour, AURORA provides you with
+            personalised recommendations based on your energy usage data.
           </p>
           <span className="text-muted-foreground text-sm">
-            This will later be automatically handled by cloud functions and is
-            only used for development and testing purposes.
+            Recommendations are updated regularly as you enter more data.
           </span>
         </div>
-        <Button
-          onClick={() => updateRecommendationsWrapper()}
-          disabled={updatingRecommendations}
-          className="w-fit"
-        >
-          Update Recommendations
-        </Button>
       </div>
     </BorderBox>
   );
